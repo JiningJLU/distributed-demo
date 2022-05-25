b8d9847357030f96698418609641790d7a730b89
jining <862217083@qq.com>
2022-05-25 23:12:36 +0800

    更新测试

--- mall-goods/src/test/java/com/samlple/mall/goods/GuavaBloomFilterTest.java
+++ mall-goods/src/test/java/com/samlple/mall/goods/GuavaBloomFilterTest.java
@@ -1,2 +1,31 @@
-package com.samlple.mall.goods;public class GuavaBloomFilterTest {
+package com.samlple.mall.goods;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GuavaBloomFilterTest {
+    private int size = 1000;
+    private double fpp = 0.001;
+
+    private BloomFilter<Integer> bloomFilter = BloomFilter.create(Funnels.integerFunnel(), size, fpp);
+
+    @Before
+    public void initBloomFilter() {
+        for (int i=0; i<size; i++) {
+            bloomFilter.put(i);
+        }
+    }
+
+    @Test
+    public void testBloomFilter() {
+        int count = 0;
+        for (int j=size; j<size + size; j++) {
+            if (bloomFilter.mightContain(j)) {
+                count++;
+                System.out.println("误判了" + j + ", 这是第" + count + "次误判");
+            }
+        }
+    }
 }
--- mall-goods/src/test/java/com/samlple/mall/goods/RedisBloomFilterTest.java
+++ mall-goods/src/test/java/com/samlple/mall/goods/RedisBloomFilterTest.java
@@ -1,2 +1,54 @@
-package com.samlple.mall.goods;public class RedisBloomFIlterTest {
+package com.samlple.mall.goods;
+
+
+import com.sample.mall.goods.MallGoodsApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {MallGoodsApplication.class})
+public class RedisBloomFilterTest {
+
+    private int size = 10000;
+    private static final String BLOOM_FILTER_NAME = "goodsBloomFilter";
+
+//    public void setRedisTemplate(RedisTemplate redisTemplate) {
+//        this.redisTemplate = redisTemplate;
+//    }
+
+    @Autowired
+    public RedisTemplate redisTemplate;
+
+    private static final Logger logger = Logger.getLogger(RedisBloomFilterTest.class.toString());
+
+    @Before
+    public void init() {
+        for (int i=0; i<size; i++) {
+            redisTemplate.opsForValue().setBit(BLOOM_FILTER_NAME, getOffset(i), true);
+        }
+    }
+
+    @Test
+    public void testRedisFilter() {
+        int count = 0;
+        for (int j=0; j<size + size; j++) {
+            if (redisTemplate.opsForValue().getBit(BLOOM_FILTER_NAME, getOffset(j)) == true) {
+                count++;
+                System.out.println("误判了" + j + ", 这是第" + count + "次误判");
+            }
+        }
+    }
+
+    private long getOffset(int i) {
+        long abs = Math.abs((BLOOM_FILTER_NAME + i).hashCode());
+        return (long) (abs % Math.pow(2, 32));
+    }
 }
